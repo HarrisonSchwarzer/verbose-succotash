75096bb69450687957ca22707d01153ad9b2cae4
realLizardPerson <harrisonschwarzer@gmail.com>
2019-05-11 19:01:32 -0500

    basic example

---
+++ callbacks/index.js
@@ -0,0 +1,12 @@
+// from https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced
+
+function first() {
+  console.log(1);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
